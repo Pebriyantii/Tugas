47d540ec0f595422d23edc4ac66579414520b07e
Pebriyantii <Pebriyanti1802@gmail.com>
2024-09-26 19:23:51 +0700

    Add files via upload

---
+++ tugas 1 pebriyanti.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    char nim = 2441037;
+    char nama[50] = "Pebriyanti";
+    char jurusan[50] = "Teknik Informatika";
+
+    printf("NIM: %d\n", nim);
+    printf("Nama: %s\n", nama);
+    printf("Jurusan: %s\n", jurusan);
+
+    return 0;
+}
